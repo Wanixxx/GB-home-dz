e51b80f12be53f67dfdc7ab640d83cf3c662dfc9
Wanixxx <46975493+Wanixxx@users.noreply.github.com>
2023-04-12 21:39:36 +0300

    Update Home_DZ_lesson_1.py

--- Home_DZ_lesson_1.py
+++ Home_DZ_lesson_1.py
@@ -1,35 +1,35 @@
-## Задание №1
-	#number = int(input("Какой сегодня число? "))
-	#words = str(input("Какой сегодня месяц? "))
-	#b = number
-	#c = words
-	#print(b, c)
+##Задание №1
+	number = int(input("Какой сегодня число? "))
+	words = str(input("Какой сегодня месяц? "))
+	b = number
+	c = words
+	print(b, c)
 
 ## Задание № 2
-#time = int(input("Укажите колличество секунд"))
-#clock = time / 3600 #Колличество часов;
-#minutes = time / 60 #Колличество минут;
-#print('Часы = '%clock, 'Минуты = '%minutes, 'Секунды = '%time)
+time = int(input("Укажите колличество секунд"))
+clock = time / 3600 #Колличество часов;
+minutes = time / 60 #Колличество минут;
+print('Часы = '%clock, 'Минуты = '%minutes, 'Секунды = '%time)
 
 ## Задание № 3
-#n = int(input("Введите число: "))
-#total = (n + int(str(n) + str(n) + int(str(n) + str(n) + str(n))))
-#print("Сумма чисел n + nn + nnn - %d" % total)
+n = int(input("Введите число: "))
+total = (n + int(str(n) + str(n) + int(str(n) + str(n) + str(n))))
+print("Сумма чисел n + nn + nnn - %d" % total)
 
 
 ## Задание №4
 
-#number = int(input("Введите положительное число: "))
-#max = number % 10
-#while number >= 1:
-#	number = number//10
-#	if number%10 > max:
-#		max = number % 10
-#	if number > 9:
-#		continue
-#	else:
-#		print(max)
-#		break
+number = int(input("Введите положительное число: "))
+max = number % 10
+while number >= 1:
+	number = number//10
+	if number%10 > max:
+		max = number % 10
+	if number > 9:
+		continue
+	else:
+		print(max)
+		break
 
 ## Задание №5 и 6
 profit = float(input("Введите выручку фирмы "))
